77cffdd6163b36104f0148c934c7890814e8fed6
bkgriffin <4809837+bkgriffin@users.noreply.github.com>
2023-08-28 09:04:57 -0400

    Created Page component.

---
+++ src/Page.js
@@ -0,0 +1,23 @@
+function Page() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src="./logo192.png" width="40px" alt="logo" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library.</li>
+                <li>React is an in demand skill.</li>
+                <li>People love using React.</li>
+                <li>I want to become a better developer through using React.</li>
+            </ol>
+            <footer>
+                <small>© 2023 Griffin development.  All rights reserved</small>
+            </footer>
+        </div>
+    )
+}
+
+export default Page;
